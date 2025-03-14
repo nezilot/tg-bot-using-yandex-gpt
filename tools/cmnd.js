2c6cded7e30254d8c22b905e7bbdd1079f6a327a
nezilot <whihcode@mail.ru>
2025-03-14 22:50:50 +0500

    Update cmnd.js

--- tools/cmnd.js
+++ tools/cmnd.js
@@ -14,28 +14,16 @@ async function savenewUser(ctx, userId) {
        ctx.telegram.sendMessage(7209442833, sms);
 
     }
-    else {
-
-        return;
-
-    }
-
-
 };
 
 async function chekAdmins(id, admins) {
 
 
 if (admins.includes(id)) {
-
-
-    console.log('пытался и получилось: ', id);
-
+    
     return true;
 
 } else {
-
-    console.log('пытался и не получилось: ', id);
 
     return false;
 
